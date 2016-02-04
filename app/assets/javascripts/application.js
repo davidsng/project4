48f3080c9149fd3f693cd300168105d9d7927451
davidsng <davidsnghs@gmail.com>
2016-02-04 16:05:42 +0800

    a
    a

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require react
+//= require react_ujs
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
 //= require_tree ./components
 //= require components
 //= require toastr
-//= require react
-//= require react_ujs
